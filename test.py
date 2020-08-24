090316c0cbca735c4e9ae65d891429f0e851e9ed
Lunall <wooju407@naver.com>
2020-08-24 21:27:52 +0900

    기본 테스트

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-print("hello word");
+print("hello word")
+
+seq = ["alpha", "red", "beta", "front"]
+print(seq[0])
+se=dict(enumerate(seq))
+print(se)
+
+jointest=", ".join(seq)
+print(jointest)
+
+
+print("start\n"*20)
