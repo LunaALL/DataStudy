4c8ac33d166db9e1c066ec10a27a0ddaab2689ec
Lunall <wooju407@naver.com>
2020-09-06 21:21:16 +0900

    외국 위키피디아 형태소분석해보기

---
+++ Quiz/Wikipideaminer.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+from collections import Counter
+from nltk.corpus import stopwords
+from nltk import LancasterStemmer
+from urllib.request import urlopen
+import nltk
+
+nltk.download('punkt')
+
+# 형태소 분류기 만들기
+ls = nltk.LancasterStemmer()
+soup = BeautifulSoup(urlopen("https://en.wikipedia.org/wiki/Data_science"), "html.parser")
+
+# 텍스트를 추출하고 토큰화한다.
+words = nltk.word_tokenize(soup.text)
+print(words)
+
+# 단어를 소문자 변환
+words = [w.lower() for w in words]
+print(words)
+
+#불용어(the 같은것) 을 제거하고 단어의 형태소 추출
+words = [ls.stem(w) for w in words if w not in stopwords.words("english") and w.isalnum()]
+print(words)
